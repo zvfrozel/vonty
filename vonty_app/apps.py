75f6a82c4784c775fcd2f73dcb417fee41374395
Abel George Mathew <zvfrozel@gmail.com>
2024-04-17 20:13:03 +0530

    Update constraints etc.

--- vonty_app/apps.py
+++ vonty_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class VontyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vonty_app'
+    verbose_name = 'Vonty'
--- vonty_app/models.py
+++ vonty_app/models.py
@@ -1,4 +1,11 @@
+"""
+Vonty models:
+1. Problem
+2. Tag
+"""
+
 from django.db import models
+from django.db.models import Q
 from django.core.validators import MaxValueValidator, StepValueValidator
 from django.contrib.auth import get_user_model
 
@@ -10,7 +17,7 @@ class Problem(models.Model):
         blank=True,
         unique=True,
         help_text=(
-            "Problem source. This must be either blank or unique."
+            "Problem source. This must be either blank or unique. "
             "e.g. IMO 2023/6"
         )
     )
@@ -21,22 +28,22 @@ class Problem(models.Model):
     )
     desc = models.CharField(
         max_length=100, help_text=(
-            "A short one-line description of the problem statement."
+            "A short one-line description of the problem statement. "
             "e.g. Fiendish inequality"
         )
     )
     aops_url = models.URLField(
         blank=True, help_text=(
-            "A link to the problem on AOPS, if it exists."
+            "A link to the problem on AOPS, if it exists. "
         )
     )
     hardness = models.PositiveIntegerField(
         null=True, blank=True,
         help_text=(
-            "Hardness of the problem according to the MOHS scale."
-            "The rating can range from 0 to 60,"
-            "and can be left blank if the problem is considered not-rateable."
-            "See https://web.evanchen.cc/upload/MOHS-hardness.pdf"
+            "Hardness of the problem according to the MOHS scale. "
+            "The rating can range from 0 to 60, "
+            "and can be left blank if the problem is considered not-rateable. "
+            "See https://web.evanchen.cc/upload/MOHS-hardness.pdf "
             "for more information."
         ),
         validators=[
@@ -50,23 +57,23 @@ class Problem(models.Model):
         null=True,
         blank=True,
         help_text=(
-            "The author, if they are a member of this database."
+            "The author, if they are a member of this database. "
             "Use this in problems you have proposed yourself, for example."
         )
     )
     proposal_date = models.DateField(
-        null=True, blank=True, help_text=(
-            "Date of problem creation."
-        )
+        null=True, blank=True, help_text="Date of problem creation."
     )
     git_url = models.URLField(
         blank=True, help_text=(
-            "Read-only link to pull the problem via git."
+            "Read-only link to pull the problem via git. "
             "See LINK TO GIT PULL DOCUMENTATION."
         )
     )
     tags = models.ManyToManyField(
-        "Tag", help_text="The list of tags associated with the problem."
+        "Tag",
+        related_name="problem_set",
+        help_text="The list of tags associated with the problem."
     )
 
 
@@ -74,26 +81,33 @@ class Tag(models.Model):
     name = models.SlugField(
         help_text="Identifier slug. e.g. anglechase."
     )
-    desc = models.CharField(
+    desc = models.TextField(
         max_length=200, help_text=(
             "Optional longer description."
         )
     )
-    is_filter = models.BooleanField(
+    use_filter = models.BooleanField(
         default=True, help_text=(
-            "Specifies whether users should use this tag to filter problems."
-            "Uncheck this for tags that are purely meant to be."
+            "Specifies whether users should use this tag to filter problems. "
+            "Uncheck this for tags that are purely meant to be "
             "used as umbrella parent tags and not as filters."
         )
     )
     parent = models.ForeignKey(
         "self",
-        blank=True,
+        null=True,
         on_delete=models.PROTECT, # TODO: Change PROTECT to SET grandparent
         related_name="children",
         help_text=(
-            "The parent tag that this tag comes under."
-            "Filtering by the parent tag filters by this tag too."
+            "The parent tag that this tag comes under. "
+            "Filtering by the parent tag filters by this tag too. "
             "If this field is blank, the is_filter option must be unchecked."
         )
     )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q()
+            ),
+        ]
