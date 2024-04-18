47f81190fef0625de9a6e88ec1e973b835d0714c
Abel George Mathew <zvfrozel@gmail.com>
2024-04-18 15:39:01 +0530

    Add __str__ methods

--- vonty_app/models.py
+++ vonty_app/models.py
@@ -8,6 +8,7 @@ from django.db import models
 from django.db.models import Q
 from django.core.validators import MaxValueValidator, StepValueValidator
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Problem(models.Model):
@@ -16,30 +17,30 @@ class Problem(models.Model):
         null=True, # avoid unique-constraint violations
         blank=True,
         unique=True,
-        help_text=(
-            "Problem source. This must be either blank or unique. "
+        help_text=_(
+            "Problem source. This must be either blank or unique. " # TODO
             "e.g. IMO 2023/6"
         )
     )
     author = models.CharField(
-        max_length=50, blank=True, help_text=(
+        max_length=50, blank=True, help_text=_(
             "e.g. Abel George Mathew (IND)"
         )
     )
     desc = models.CharField(
-        max_length=100, help_text=(
+        max_length=100, help_text=_(
             "A short one-line description of the problem statement. "
             "e.g. Fiendish inequality"
         )
     )
     aops_url = models.URLField(
-        blank=True, help_text=(
+        blank=True, help_text=_(
             "A link to the problem on AOPS, if it exists. "
         )
     )
     hardness = models.PositiveIntegerField(
         null=True, blank=True,
-        help_text=(
+        help_text=_(
             "Hardness of the problem according to the MOHS scale. "
             "The rating can range from 0 to 60, "
             "and can be left blank if the problem is considered not-rateable. "
@@ -47,8 +48,8 @@ class Problem(models.Model):
             "for more information."
         ),
         validators=[
-            MaxValueValidator(60, "MOHS rating cannot exceed 60"),
-            StepValueValidator(5, "MOHS rating must be a multiple of 5")
+            MaxValueValidator(60, _("MOHS rating cannot exceed 60")),
+            StepValueValidator(5, _("MOHS rating must be a multiple of 5"))
         ]
     )
     proposer = models.ForeignKey(
@@ -56,16 +57,16 @@ class Problem(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        help_text=(
+        help_text=_(
             "The author, if they are a member of this database. "
             "Use this in problems you have proposed yourself, for example."
         )
     )
     proposal_date = models.DateField(
-        null=True, blank=True, help_text="Date of problem creation."
+        null=True, blank=True, help_text=_("Date of problem creation.")
     )
     git_url = models.URLField(
-        blank=True, help_text=(
+        blank=True, help_text=_(
             "Read-only link to pull the problem via git. "
             "See LINK TO GIT PULL DOCUMENTATION."
         )
@@ -73,21 +74,22 @@ class Problem(models.Model):
     tags = models.ManyToManyField(
         "Tag",
         related_name="problem_set",
-        help_text="The list of tags associated with the problem."
+        help_text=_("The list of tags associated with the problem.")
     )
+
+    def __str__(self):
+        return self.desc
 
 
 class Tag(models.Model):
     name = models.SlugField(
-        help_text="Identifier slug. e.g. anglechase."
+        help_text=_("Identifier slug. e.g. angle-chase")
     )
     desc = models.TextField(
-        max_length=200, help_text=(
-            "Optional longer description."
-        )
+        max_length=200, help_text=_("Optional longer description.")
     )
     use_filter = models.BooleanField(
-        default=True, help_text=(
+        default=True, help_text=_(
             "Specifies whether users should use this tag to filter problems. "
             "Uncheck this for tags that are purely meant to be "
             "used as umbrella parent tags and not as filters."
@@ -96,18 +98,46 @@ class Tag(models.Model):
     parent = models.ForeignKey(
         "self",
         null=True,
+        blank=True,
         on_delete=models.PROTECT, # TODO: Change PROTECT to SET grandparent
         related_name="children",
-        help_text=(
+        help_text=_(
             "The parent tag that this tag comes under. "
             "Filtering by the parent tag filters by this tag too. "
-            "If this field is blank, the is_filter option must be unchecked."
+            "If this field is blank, the use filter option must be unchecked."
         )
     )
+
+    def __str__(self):
+        return self.name.replace("-", " ").replace("_", " ").title()
+
+    def check_ancestor(self, tag):
+        """
+        Check whether ``tag'' is an ancestor of ``self''.
+        A tag is an ancestor of itself.
+        """
+        if tag == self:
+            return True
+        if self.parent is None:
+            return False
+        else:
+            return self.parent.check_ancestor(tag)
+
+    def check_descendant(self, tag):
+        """
+        Check whether ``tag'' is a descendant of ``self''.
+        A tag is a descendant of itself.
+        """
+        return tag.check_ancestor(self)
 
     class Meta:
         constraints = [
             models.CheckConstraint(
-                check=Q()
+                check=Q(use_filter=False) | Q(parent__isnull=False),
+                name="filter_implies_parent",
+                violation_error_message=_(
+                    "Tag meant to be used as a filter "
+                    "must have a parent tag."
+                )
             ),
         ]
